12ba8ec46b99b7ef89dfd8970323f0daf2e8b77e
gwatsin <gwatsinthong@gmail.com>
2024-08-25 15:20:46 +0530

    Experimentation

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+const fs = require("fs");
+
+fs.writeFile("Message.text", "Hello Node", (er) => {
+  if (er) throw er;
+    console.log("File Created");
+});
+
+fs.readFile("Message.text", "utf-8", (er, data) => {
+  if (er) throw er;
+  console.log(data);
+})
