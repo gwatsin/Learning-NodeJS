5c33f5fcf782f34639c1672c7e0bece65f82a053
gwatsin <gwatsinthong@gmail.com>
2024-08-26 13:26:40 +0530

    Final Commit

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const fs = require("fs");
+// const fs = require("fs");
 
-fs.writeFile("Message.text", "Hello Node", (er) => {
-  if (er) throw er;
-    console.log("File Created");
-});
+// fs.writeFile("Message.text", "Hello Node", (er) => {
+//   if (er) throw er;
+//     console.log("File Created");
+// });
 
-fs.readFile("Message.text", "utf-8", (er, data) => {
-  if (er) throw er;
-  console.log(data);
-})
+// fs.readFile("Message.text", "utf-8", (er, data) => {
+//   if (er) throw er;
+//   console.log(data);
+// })
+
+
+// var generateName = require("sillyname");
+
+import generateName from "sillyname";
+var sillyName = generateName();
+
+console.log('My name is', sillyName);
+
+import {randomSuperhero} from "superheroes";
+var randomSupe = randomSuperhero();
+
+console.log('My superhero name is', randomSupe);
